0258cdf29547efcbfb39ef38f6b72f6c74226223
Sam <student979@live.com>
2020-10-24 02:39:11 -0400

    add recycler view for movies

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,26 +16,37 @@ import org.json.JSONObject;
 
 import okhttp3.Headers;
 
-import
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    RecyclerView rvMovies;
+    ArrayList<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        movies = new ArrayList<>();
+
+        rvMovies = findViewById(R.id.rvMovies);
+        final MovieAdapter adapter = new MovieAdapter(this, movies);
+        rvMovies.setAdapter(adapter);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rvMovies.setLayoutManager(layoutManager);
+
 
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+        client.get("https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed", new JsonHttpResponseHandler() {
 
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 try{
                     JSONArray results = json.jsonObject.getJSONArray(("results"));
-//                    movies.addAll(Movie.fromJsonArray(results));
-//                    adapter.notifyDataSetChanged();
-                    Log.e("MainActivity", results.toString()+"");
+                    for (int i = 0; i < results.length(); i++) {
+                        movies.add(new Movie(results.getJSONObject(i)));
+                    }
+                    adapter.notifyDataSetChanged();
+                    Log.e("MainActivity", movies.toString()+"");
                 } catch(Exception e){
                     Log.e("MainActivity", e.toString());
                 }
